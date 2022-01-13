fb82e48fc125297ff5565cf6fe63ebf02d3f791a
KatoIT <nguyenvananymnb@gmail.com>
2022-01-13 08:51:20 +0700

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int a = 50000;
+        double sum1 = 0.0;
+        double sum2 = 0.0;
+        for (int i = 1; i <= a; i++) {
+            sum1 += 1.0 / i;
+        }
+        for (int i = a; i > 0; i--) {
+            sum2 += 1.0 / i;
+        }
+        System.out.println(sum1 - sum2);
     }
 }
