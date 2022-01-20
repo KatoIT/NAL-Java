b262e9a599e14e702f5173fceb35f3164a96b8e4
KatoIT <nguyenvananymnb@gmail.com>
2022-01-20 13:51:33 +0700

    update

---
+++ src/com/company/Test.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+
+        try {
+            int x = 0;
+            int y = 5 / x;
+        }catch (ArithmeticException e) {
+            System.out.println("Arithmetic");
+        }catch (Exception ae) {
+            System.out.println("Exception");
+        }
+        System.out.println("finished");
+    }
+
+
+    public static void badMethod() {
+        throw new Error();
+    }
+}
