fc92488a11ae17c85a7bc11d03ca243673cba758
KatoIT <nguyenvananymnb@gmail.com>
2022-01-27 14:44:11 +0700

    update

---
+++ src/factory/Cat.java
@@ -0,0 +1,8 @@
+package factory;
+
+public class Cat extends Animal {
+    @Override
+    public String makeSound() {
+        return "Meow";
+    }
+}
---
+++ src/object/pool/TaxiNotFoundException.java
@@ -0,0 +1,8 @@
+package object.pool;
+
+public class TaxiNotFoundException extends RuntimeException {
+
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}
